eea77f8e4647aabb83aadb8147d7ebd14cea2a1a
Younes <younes.ghoniem@gmail.com>
2022-10-11 14:11:21 +0200

    graphql tutorial end

--- movie/movie.py
+++ movie/movie.py
@@ -1,6 +1,6 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
 
@@ -11,11 +11,24 @@ app = Flask(__name__)
 ### ADD THINGS HERE
 #
 ###
+type_defs = load_schema_from_path('movie.graphql')
+query = QueryType()
+mutation = MutationType()
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+movie = ObjectType('Movie')
+actor = ObjectType('Actor')
+movie.set_field('actors', r.resolve_actors_in_movie)
+actor.set_field('films', r.resolve_movies_in_actor)
+query.set_field('movie_with_id', r.movie_with_id)
+query.set_field('actor_with_id', r.actor_with_id)
+schema = make_executable_schema(type_defs, query, movie, mutation, actor)
+
 
 # root message
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
+
 
 #####
 # graphql entry points
@@ -23,19 +36,23 @@ def home():
 @app.route('/graphql', methods=['GET'])
 def playground():
     return PLAYGROUND_HTML, 200
-    
+
+
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
     data = request.get_json()
     success, result = graphql_sync(
-    schema,
-    data,
-    context_value=None,
-    debug=app.debug
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
     )
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
+
 if __name__ == "__main__":
-    print("Server running in port %s"%(PORT))
-    app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT, debug=True)
+
+# does it work??
--- movie/resolvers.py
+++ movie/resolvers.py
@@ -1,2 +1,46 @@
 import json
 
+
+def movie_with_id(_, info, _id):
+    with open('{}/data/movies.json'.format("."), 'r') as f:
+        movies = json.load(f)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                return movie
+
+
+def actor_with_id(_, info, _id):
+    with open('{}/data/actors.json'.format("."), 'r') as f:
+        actors = json.load(f)
+        for actor in actors['actors']:
+            if actor['id'] == _id:
+                return actor
+
+
+def resolve_actors_in_movie(movie, info):
+    with open('{}/data/actors.json'.format("."), "r") as file:
+        data = json.load(file)
+        actors = [actor for actor in data['actors'] if movie['id'] in actor['films']]
+        return actors
+
+
+def resolve_movies_in_actor(actor, info):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        data = json.load(file)
+        movies = [movie for movie in data['movies'] if actor['id'] in movie['actors']]
+        return movies
+
+
+def update_movie_rate(_, info, _id, _rate):
+    new_movie = {}
+    new_movies = {}
+    with open('{}/data/movies.json'.format("."), 'r') as f:
+        movies = json.load(f)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                movie['rating'] = _rate
+                new_movie = movie
+                new_movies = movies
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(new_movies, wfile)
+    return new_movie
